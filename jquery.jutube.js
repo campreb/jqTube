7e2a4239da4c9b1f65a2aa6dbc693cf7d448b091
Cameron Prebble <campreb@cameron-prebbles-macbook.local>
2009-09-09 22:22:25 +1200

    Added jquery plugin

---
+++ jquery.jutube.js
@@ -0,0 +1,15 @@
+(function($){
+    $.fn.extend({
+       juTube:function(id,options){
+           var defaults = {
+
+           };
+           var options = $.extend(defaults, options);
+
+           return this.each(function(){
+               var obj = $(this);
+               
+           });
+       }
+    });
+})(jQuery);
